b475d3d8712a7e9632a2a5349fa73540765d9a92
MC <michelcapelle@gmail.com>
2026-01-11 22:37:13 +0100

    fixes

--- flipke-iii-dun-broave/src/app/pages/timeline/timeline.component.ts
+++ flipke-iii-dun-broave/src/app/pages/timeline/timeline.component.ts
@@ -83,7 +83,7 @@ export class TimelineComponent implements OnInit {
       console.log(`Loaded data for year ${year}: ${allPersons.length} persons`);
       setTimeout(() => {
         this.loadYearData(year + 1);
-      }, 50);
+      }, 25);
     } catch (error) {
       console.warn(`Failed to load year ${year}`);
       this.loading = false;
